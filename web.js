5f5477ca19014adebc68449605cb6863805230ac
Gergely Imreh <imrehg@gmail.com>
2012-11-29 12:12:57 +0800

    insight

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var url = require('url');
 var app = express();
 
 var linkedin = {};
@@ -7,6 +8,15 @@ linkedin.apikey = process.env.LINKEDIN_API;
 app.use(express.static(__dirname+'/public'));
 
 app.get('/', function(req, res) {
+    res.render('index.ejs', {
+	layout:false,
+	linkedin: linkedin,
+	host: req.header('host'),
+	url: req.url
+    });
+});
+
+app.get('/test', function(req, res) {
     res.render('index.ejs', {
 	layout:false,
 	linkedin: linkedin
